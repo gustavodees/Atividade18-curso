547108b3fbf49d8f73a0b9172a9594bc9ba86803
ChocolateKest <gustavoemartins@gmail.com>
2025-06-23 20:46:22 -0300

    First commit

---
+++ src/main/java/principal/Main.java
@@ -0,0 +1,19 @@
+package principal;
+
+import javax.swing.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor:"));
+
+        while(x !=0){
+
+        x = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor:"));
+
+        }
+
+
+    }
+}
